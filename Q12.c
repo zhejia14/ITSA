ab83715630d2c524f8e0a20ed5a0f6d392b17248
zhejia14 <106137353+zhejia14@users.noreply.github.com>
2022-11-30 15:01:41 +0800

    Create Q12.c

---
+++ Q12.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int fun(int n){
+    if(n==0 || n==1)return n+1;
+    else return fun(n-1)+fun(n/2);
+}
+int main(){
+    int in;
+    scanf("%d", &in);
+    in=fun(in);
+    printf("%d\n", in);
+}
