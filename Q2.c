e24fa581a8636bbe49f4c618326a19c47711dcf2
zhejia14 <106137353+zhejia14@users.noreply.github.com>
2022-12-13 16:37:01 +0800

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -4,8 +4,8 @@ int main() {
     int input;
     double output;
     while(scanf("%d", &input)!=EOF){
-        output=input*1.6;
-        printf("%.1lf\n", output);
+        output=input*1.6;//1英哩=1.6公里
+        printf("%.1lf\n", output);//輸出到小數點後一位
     }
     return 0;
 }
