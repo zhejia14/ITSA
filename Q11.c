f4ed6fa348ae7f351abb1f1e997af233242eb4af
zhejia14 <106137353+zhejia14@users.noreply.github.com>
2022-12-13 16:33:23 +0800

    Update Q11.c

--- Q11.c
+++ Q11.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {
     int row, col;
-    while (scanf("%d %d", &row, &col)!=EOF) {
+    while (scanf("%d %d", &row, &col)!=EOF) {//input matrix
         int array[row][col];
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 scanf("%d", &array[i][j]);
             }
         }
-        for(int i=0;i<col;i++){
+        for(int i=0;i<col;i++){//轉制矩陣並印出結果
             for(int j=0;j<row;j++){
                if(j!=row-1) printf("%d ", array[j][i]);
                else printf("%d", array[j][i]);
